24c90e518fde597ef005b0f080efd39cc25262f9
Chris Cooper <ccooper09@qub.ac.uk>
2016-03-08 22:05:20 +0000

    Post processing opening and closing methods added Test class added for post processing

--- pipeline/src/main/java/pipeline/postprocessing/PostprocessingImpl.java
+++ pipeline/src/main/java/pipeline/postprocessing/PostprocessingImpl.java
@@ -1,11 +1,73 @@
 package pipeline.postprocessing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pipeline.segmentation.SegmentationImpl;
+import qub.visionsystem.ImageOp;
+import util.ConfigurationUtils;
+
 import java.awt.image.BufferedImage;
 
 public class PostprocessingImpl implements IPostprocessing{
+
+    /**
+     * The logger for this class.
+     */
+    private static final Logger log = LoggerFactory.getLogger(PostprocessingImpl.class);
+
+    /**
+     * The size of the mask to use
+     */
+    private int maskSize = 3;
+
+    /**
+     * Constructs a new instance of the PostprocessingImpl pipeline block.
+     */
+    public PostprocessingImpl() {
+        maskSize = Integer.parseInt(ConfigurationUtils.loadProperty("maskSize"));
+    }
+
+    /**
+     * Performs an opening.
+     * @param source The input image.
+     * @return The Image after opening has been performed.
+     */
+    public BufferedImage PerformOpening(BufferedImage source){
+        return ImageOp.open(source, maskSize);
+    }
+
+    /**
+     * Performs a closing.
+     * @param source The input image.
+     * @return The Image after opening has been performed.
+     */
+    public BufferedImage PerformClosing(BufferedImage source){
+        return ImageOp.close(source, maskSize);
+    }
+
+    /**
+     * Performs an opening then a closing.
+     * @param source The input image.
+     * @return The Image after opening has been performed.
+     */
+    public BufferedImage PerformOpeningThenClosing(BufferedImage source){
+         source = ImageOp.open(source, maskSize);
+        return ImageOp.close(source, maskSize);
+    }
+
+    /**
+     * Performs a closing then an opening.
+     * @param source The input image.
+     * @return The Image after opening has been performed.
+     */
+    public BufferedImage PerformClosingThenOpening(BufferedImage source){
+        source = ImageOp.close(source, maskSize);
+        return ImageOp.open(source, maskSize);
+    }
 
     @Override
     public BufferedImage performPostProcessing(BufferedImage bufferedImage) {
         return null;
     }
+
 }
---
+++ pipeline/src/test/java/pipeline/postprocessing/PostProcessingImplTest.java
@@ -0,0 +1,7 @@
+package pipeline.postprocessing;
+
+/**
+ * Created by ccoop on 08/03/2016.
+ */
+public class PostProcessingImplTest {
+}
