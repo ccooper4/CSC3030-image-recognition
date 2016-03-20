747085200c0f53911e01a6e589d12f7a2fd8a3c1
Andrew Fletcher <afletcher04@qub.ac.uk>
2016-03-20 17:02:44 +0000

    Adding logic to save results to a JSON file.

--- ui/src/main/java/ui/ResultTableModel.java
+++ ui/src/main/java/ui/ResultTableModel.java
@@ -5,12 +5,16 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.stream.JsonReader;
+import util.ConfigurationUtils;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Vector;
 
 /**
  * An override of the DefaultTableModel that provides the column names and other specifics for the results view.
@@ -19,41 +23,85 @@ public class ResultTableModel extends DefaultTableModel {
 
     private static String columnNames[] = { "Date", "Preprocessing", "Segmentation", "Post Processing", "Feature Extraction", "Classifier", "Score" };
 
+    private String jsonPath = ConfigurationUtils.loadProperty("resultFile");
+
+    private Gson jsonBuilder;
+
     /**
-     * Constructs a blank table model.
+     * Constructs the table model with the specified JSON data file.
      */
     public ResultTableModel() {
 
         super(null, columnNames);
-    }
 
-    /**
-     * Constructs the table model with the specified JSON data file.
-     * @param jsonPath
-     */
-    public ResultTableModel(String jsonPath) {
-
-        super(null, columnNames);
-
-        Gson gson = new GsonBuilder().create();
+        jsonBuilder = new GsonBuilder().create();
 
         FileReader reader = null;
 
         try {
             reader = new FileReader(jsonPath);
         } catch (FileNotFoundException e) {
+            return;
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
 
-        BufferedReader bufferedReader = new BufferedReader(reader);
+        String[][] fileData = reader == null ? null : jsonBuilder.fromJson(reader, String[][].class);
 
-        String[][] fileData = gson.fromJson(bufferedReader, String[][].class);
+        if (fileData != null) {
 
-        for (String[] row: fileData) {
+            //Sort by the date - need to allow for non-linear insert following merge.
+            Arrays.sort(fileData, new Comparator<String[]>() {
+                @Override
+                public int compare(final String[] a, final String[] b) {
+                    final String timeA = a[0];
+                    final String timeB = b[0];
+                    return timeA.compareTo(timeB);
+                }
+            });
 
-            this.addRow(row);
 
+            for (String[] row : fileData) {
+                super.addRow(row);
+            }
         }
+    }
+
+    /**
+     * Adds a new row to the table.
+     * @param rowData
+     */
+    @Override
+    public void addRow(Object[] rowData) {
+
+        super.addRow(rowData);
+
+        String[][] jsonData = new String[this.getRowCount()][columnNames.length];
+
+        //Get each vector in the data vector
+        for (int row = 0; row < this.getRowCount(); row++)
+        {
+            for (int col = 0; col < columnNames.length; col++)
+            {
+                jsonData[row][col] = this.getValueAt(row,col).toString();
+            }
+        }
+
+        String json = jsonBuilder.toJson(jsonData);
+
+        try {
+            BufferedWriter writer = Files.newBufferedWriter(Paths.get(jsonPath), StandardCharsets.UTF_8);
+
+            writer.write(json);
+
+            writer.flush();
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
