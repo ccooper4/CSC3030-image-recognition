535b5ed4762846f696a104565176cfddce4dd7e5
Chris Cooper <ccooper09@qub.ac.uk>
2016-03-23 16:15:00 +0000

    Update classification class, needs test set populated

--- pipeline/src/test/java/pipeline/classification/ClassificationImplTest.java
+++ pipeline/src/test/java/pipeline/classification/ClassificationImplTest.java
@@ -1,9 +1,16 @@
 package pipeline.classification;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import pipeline.featureextraction.FeatureExtractionImpl;
+import pipeline.featureextraction.FeaturePayload;
+import util.ConfigurationUtils;
 import util.ResourceUtils;
+import util.StringConstants;
 import util.image.ImageUtils;
+
+import java.awt.image.BufferedImage;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,13 +18,30 @@ import static org.junit.Assert.assertEquals;
  * Created by Chris on 23/03/2016.
  */
 public class ClassificationImplTest {
+    public BufferedImage testingImage;
+    public FeatureExtractionImpl featureExtraction;
+    public ClassificationImpl classification;
+
+    public FeaturePayload featurePayload;
+    public final int area = 12605;
+    public final int perimeter = 1075;
+    public final int compactness = 91;
+    public String expectedClassification = "Apple";
+
     @Before
     public void loadTestingImage() {
-
+        testingImage = ImageUtils.readInImage(ResourceUtils.getResourcePathAsString("segmented.png"));
+        featureExtraction = new FeatureExtractionImpl();
+        featurePayload = new FeaturePayload();
+        featurePayload.setArea(area);
+        featurePayload.setPerimeter(perimeter);
+        featurePayload.setCompactness(compactness);
+        ConfigurationUtils.writeProperty(StringConstants.K_VALUE, "3");
+        classification = new ClassificationImpl();
     }
 
     @Test
     public void testPerformClassification() {
-
+        //assertEquals(expectedClassification, classification.classify(featurePayload));
     }
 }
