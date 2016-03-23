9a515ded3f32f01663fb051f204e27e4ac9220bb
Chris Cooper <ccooper09@qub.ac.uk>
2016-03-23 03:12:35 +0000

    Added test class for classification

---
+++ pipeline/src/test/java/pipeline/classification/ClassificationImplTest.java
@@ -0,0 +1,23 @@
+package pipeline.classification;
+
+import org.junit.Before;
+import org.junit.Test;
+import util.ResourceUtils;
+import util.image.ImageUtils;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Chris on 23/03/2016.
+ */
+public class ClassificationImplTest {
+    @Before
+    public void loadTestingImage() {
+
+    }
+
+    @Test
+    public void testPerformClassification() {
+
+    }
+}
